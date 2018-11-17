aeb452b4c828c8c081b11b5aa3e61b40bca25ec8
Ehis Ojielu <eojielu@gmail.com>
2018-11-17 16:35:12 -0600

    Replace stimulus’s with vuejs

--- public/js/main.js
+++ public/js/main.js
@@ -1,40 +1,27 @@
-(function () {
-    const application = Stimulus.Application.start();
-    const IpsumController = class extends Stimulus.Controller {
+// Vue Application
 
-        initialize() {
-            this.sentences = 21;
+window.onload = function() {
+  const App = new Vue({
+    el: "#app",
+    data: {
+      sentences: "",
+      body: ""
+    },
+    created() {
+      this.generate();
+    },
+    methods: {
+      async generate(event) {
+        const response = await axios({
+          method: "GET",
+          url: `/v1/issa-ipsum?sentences=${this.sentences}`
+        });
 
-            if (document.queryCommandSupported("copy")) {
-                this.element.classList.add('clipboard--supported');
-            }
-        }
-
-        copy() {
-            this.body.select();
-            document.execCommand('copy');
-        }
-
-        get body() {
-            return this.targets.find('body');
-        }
-
-        set body(value) {
-            this.data.set('body', value);
-            this.targets.find('body').value = value;
-        }
-
-        async generate() {
-            console.log(`You're requesting ${this.sentences} sentences`);
-            const { data: { body } } = await axios({
-                method: 'GET',
-                url: `/v1/issa-ipsum?sentences=${this.sentences}`
-            })
-
-            this.body = body;
-        }
+        const {
+          data: { body }
+        } = response;
+        this.body = body;
+      }
     }
-
-    // register controllers
-    application.register('ipsum', IpsumController);
-})();
+  });
+};
